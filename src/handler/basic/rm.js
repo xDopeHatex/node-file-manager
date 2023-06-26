a72753758fae0a7b53bde0f8040c7183f5baa641
Ivan Gorokhov <xgorokhovx@gmail.com>
2023-06-27 00:22:28 +0300

    feat: refactor

--- src/handler/basic/rm.js
+++ src/handler/basic/rm.js
@@ -1,18 +1,17 @@
-import { promises as fs } from 'fs';
+import { promises as fs } from "fs";
 import { resolve } from "path";
-import { dispCurrentDir } from '../../helper/index.js';
+import { dispCurrentDir } from "../../helper/index.js";
 
 export const rmHandler = async (fileName) => {
-    try {
-        const currentPath = resolve(process.cwd());
-        const filePath = resolve(currentPath, String(fileName));
+  try {
+    const filePath = resolve(resolve(process.cwd()), String(fileName));
 
-        await fs.access(filePath, fs.constants.F_OK);
+    await fs.access(filePath, fs.constants.F_OK);
 
-        await fs.unlink(filePath);
-        dispCurrentDir();
-    } catch (err) {
-        console.error('Operation failed');
-        dispCurrentDir();
-    }
-  };
+    await fs.unlink(filePath);
+    dispCurrentDir();
+  } catch (err) {
+    console.error("Operation failed");
+    dispCurrentDir();
+  }
+};
